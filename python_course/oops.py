19658d431dfc3a5e40a88206fbbb3ee2e9ee04f5
waizeshaikh123 <140107406+waizeshaikh123@users.noreply.github.com>
2024-05-08 12:37:58 +0500

    python

--- python_course/oops.py
+++ python_course/oops.py
@@ -1,17 +1,47 @@
-class Person:
-    name = "waize"
-    s_name = "shaikh"
+# class Person:
+#     name = "waize"
+#     s_name = "shaikh"
+#     def info(self):
+#         print(f"{self.name} is a {self.s_name}")
+
+
+
+# a = Person()
+# b = Person()
+# a.name = "zohaib"
+# a.s_name = "mangwano"
+
+# b.name = "sahil"
+# b.s_name = "ghori"
+# a.info()
+# b.info()
+
+class Person():
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
     def info(self):
-        print(f"{self.name} is a {self.s_name}")
+        print(f"My name is {self.name} my age is {self.age}")
+        
+a = Person("waize",20)
+b = Person("zohaib",22)
 
-
-
-a = Person()
-b = Person()
-a.name = "zohaib"
-a.s_name = "mangwano"
-
-b.name = "sahil"
-b.s_name = "ghori"
 a.info()
 b.info()
+
+def greet(fx):
+    def mfx(*args,**kwargs):
+        print("Hy hope You Enjoy")
+        fx(*args, **kwargs)
+        print("See you soon bye...")
+    return mfx
+
+@greet
+def hello():
+    print("hello world")
+hello()
+
+@greet
+def add(a,b):
+    print(a+b)
+add(3,4)
